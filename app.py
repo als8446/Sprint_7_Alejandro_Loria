27fa7dc7321057cd1cad927eacfa8988bc70d403
als8446 <loria8446@icloud.com>
2025-03-25 17:04:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if hist_button:
     st.plotly_chart(fig, use_container_width=True)
 
 show_scatter = st.checkbox('Mostrar gráfico de dispersión')
-if show_scatter:  # Cambié scatter_button por show_scatter
+if show_scatter:
     st.write('Creación de un gráfico de dispersión para el precio vs millaje')
     fig = px.scatter(car_data, x="odometer", y="price", title="Relación entre millaje y precio")
     st.plotly_chart(fig, use_container_width=True)
